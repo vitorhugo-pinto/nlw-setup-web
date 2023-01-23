fbe4e5c14ee8dfe29463ecacae142a46863cc78e
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-23 08:57:02 -0300

    Header component with svg logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
-import { Habit } from "./components/Habit";
+import { Header } from "./components/Header";
 import "./styles/global.css";
 
-function App() {
-  return <Habit />;
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+      </div>
+    </div>
+  );
 }
-
-export default App;
