256f065ee9cfb2af63e80eee8feb0447cddc64a1
Vitor Hugo Pinto <vhpinto1@gmail.com>
2023-01-23 10:16:57 -0300

    customized grid template for 7 rows

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
       colors: {
         background_default: "#09090A",
       },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
     },
   },
   plugins: [],
